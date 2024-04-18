e3e47426c803ea454357ed04286a86674cacda9c
Gohar ayub <ga23aac@herts.ac.uk>
2024-04-18 16:54:42 +0100

    Jnit Unit Testing added for every class with just single function of every class

---
+++ src/test/Change_Cancel_BookingTest.java
@@ -0,0 +1,12 @@
+package test;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Change_Cancel_BookingTest {
+
+    @Test
+    void cancelBooking() {
+    }
+}
